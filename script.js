0ee15275f72995eb23a912f8f989de016f39a5a1
visa-nadra-gov-pk <ss.sahir2017@gmail.com>
2023-09-24 10:40:17 +0430

    Add logic to dice role and add score the player

--- script.js
+++ script.js
@@ -3,8 +3,34 @@
 // Selecting Elemetns
 const player0Element = document.querySelector('#score--0');
 const player1Element = document.querySelector('#score--1');
-const dice = document.querySelector('.dice'); 
+const dice = document.querySelector('.dice');
+const rollDiceBtn = document.querySelector('.btn--roll');
+const playerOneCurrentScore = document.querySelector('#current--0');
 
+// Cleared defult data of the score and dice
 player0Element.textContent = 0;
 player1Element.textContent = 0;
 dice.classList.add('hidden');
+
+// Variable to hold players score
+let playerScore = 0;
+
+// add logic to roll th dice
+rollDiceBtn.addEventListener('click', function(){
+    // Generate rondom number from 1 to 6
+    const diceNumber = Math.trunc(Math.random()*6) +1;
+
+    // add dice image according to the generated number
+    dice.classList.remove('hidden');
+   console.log( dice.src = `dice-${diceNumber}.png`);
+
+//    add score to the player
+    if(diceNumber !== 1)
+    {
+        playerScore += diceNumber;
+        console.log(playerScore)
+        playerOneCurrentScore.textContent = playerScore;
+    }
+
+
+});
