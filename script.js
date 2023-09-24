b684a92b548d986d77659b8b62438e56d4eb62be
visa-nadra-gov-pk <ss.sahir2017@gmail.com>
2023-09-24 10:00:26 +0430

    Element selecting and removing player numbers

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting Elemetns
+const player0Element = document.querySelector('#score--0');
+const player1Element = document.querySelector('#score--1');
+const dice = document.querySelector('.dice'); 
+
+player0Element.textContent = 0;
+player1Element.textContent = 0;
+dice.classList.add('hidden');
